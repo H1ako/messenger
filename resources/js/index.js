bd2f18e6be169ac4be260654cde7804f765d868e
H1ako <nikita-sobolev-wd@yandex.ru>
2021-09-18 00:02:15 +0400

    add project

---
+++ resources/js/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+class MessageAreaComponent extends React.Component{
+
+}
+
+class MessageComponent extends React.Component{
+
+
+    render() {
+        return 
+    }
+}
+
+
+ReactDOM.render(
+    React.createElement(MessageAreaComponent, null),
+    document.getElementById('message-area')
+)
